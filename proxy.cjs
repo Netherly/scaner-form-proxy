31749b3a9799de743d91e55f3af759f83e10da7e
Netherly <pinkjiq@gmail.com>
2025-05-08 17:44:36 +0300

    Update proxy.cjs

--- proxy.cjs
+++ proxy.cjs
@@ -3,10 +3,12 @@ const cors = require("cors");
 const fetch = require("node-fetch");
 
 const app = express();
+const port = process.env.PORT || 3001;  
 app.use(cors());
 app.use(express.text({ type: "text/plain", limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 
+// Маршрут для формы 1
 app.post("/form1", async (req, res) => {
   try {
     const response = await fetch("https://script.google.com/macros/s/AKfycbyTKZ4z840J6leWUKr6SukYQsuooRh4kINSsi7pwY2MCQ-yfpF0Ckqkg7Uwo3tfj2uf/exec", {
@@ -31,6 +33,8 @@ app.post("/form1", async (req, res) => {
     res.status(500).json({ success: false, message: "Ошибка на сервере" });
   }
 });
+
+// Маршрут для формы 2
 app.post("/form2", async (req, res) => {
   try {
     const response = await fetch(
@@ -58,6 +62,8 @@ app.post("/form2", async (req, res) => {
     res.status(500).json({ success: false, message: "Ошибка на сервере" });
   }
 });
+
+// Маршрут для формы 3
 app.post("/form3", async (req, res) => {
   try {
     const response = await fetch(
@@ -81,10 +87,12 @@ app.post("/form3", async (req, res) => {
 
     res.json(data);
   } catch (error) {
-    console.error("Ошибка при обращении к скрипту (form2):", error);
+    console.error("Ошибка при обращении к скрипту (form3):", error);
     res.status(500).json({ success: false, message: "Ошибка на сервере" });
   }
 });
+
+// Маршрут для формы 4
 app.post("/form4", async (req, res) => {
   console.log("Получен запрос на /form4 с телом:", req.body);
   try {
@@ -109,9 +117,11 @@ app.post("/form4", async (req, res) => {
 
     res.json(data);
   } catch (error) {
-    console.error("Ошибка при обращении к скрипту (form2):", error);
+    console.error("Ошибка при обращении к скрипту (form4):", error);
     res.status(500).json({ success: false, message: "Ошибка на сервере" });
   }
 });
 
-app.listen(3001, () => console.log("Proxy server running on http://localhost:3001"));
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
